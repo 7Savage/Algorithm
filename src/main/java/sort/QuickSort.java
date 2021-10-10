cca41807ffd53bca70d7a1566dd8d15d958e8887
AMD-PC <wjw1445331723@gmail.com>
2021-10-10 20:59:13 +0800

    冒泡排序、选择排序OK

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -1,5 +1,29 @@
 package sort;
 
-public class QuickSort {
+import java.util.Arrays;
 
+public class QuickSort {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            boolean flag = true;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                } else {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 2, 5, 3, 6, 2, 1, 7, 4};
+        sort(arr);
+    }
 }
--- src/main/java/sort/SelectionSort.java
+++ src/main/java/sort/SelectionSort.java
@@ -1,4 +1,27 @@
 package sort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 2, 5, 3, 6, 2, 1, 7, 4};
+        sort(arr);
+    }
 }
