6a63e6202083d18474832e99672317003f6fcca1
AMD-PC <wjw1445331723@gmail.com>
2021-11-08 20:36:32 +0800

    6

--- src/main/java/sort/SelectionSort.java
+++ src/main/java/sort/SelectionSort.java
@@ -2,6 +2,11 @@ package sort;
 
 import java.util.Arrays;
 
+/**
+ * 1. 在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
+ * 2. 从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ * 3. 重复第二步，直到所有元素均排序完毕。
+ */
 public class SelectionSort {
     public static void sort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
