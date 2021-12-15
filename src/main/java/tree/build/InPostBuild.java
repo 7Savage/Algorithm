02a5ec458d85383d5ee6e1a946c216dd8f30f204
AMD-PC <wjw1445331723@gmail.com>
2021-12-15 21:51:56 +0800

    遍历建树

---
+++ src/main/java/tree/build/InPostBuild.java
@@ -0,0 +1,9 @@
+package tree.build;
+
+import tree.TreeNode;
+
+public class InPostBuild {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return null;
+    }
+}
---
+++ src/main/java/tree/build/PreInBuild.java
@@ -0,0 +1,9 @@
+package tree.build;
+
+import tree.TreeNode;
+
+public class PreInBuild {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return null;
+    }
+}
