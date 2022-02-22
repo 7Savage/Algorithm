46d617c499a52639ecf396be74d98f5edad8211e
7Savage <wjw1445331723@gmail.com>
2022-02-22 20:56:26 +0800

    更新了迪杰斯特拉，但是没有写出来

--- src/main/java/tree/order/BFS.java
+++ src/main/java/tree/order/BFS.java
@@ -9,9 +9,7 @@ import java.util.List;
 
 public class BFS {
     public void levelOrder(TreeNode root) {
-        List<Integer> level = new ArrayList<>();
         Deque<TreeNode> queue = new LinkedList<>();
-
         if (root == null) {
             return;
         }
@@ -20,12 +18,39 @@ public class BFS {
             TreeNode node = queue.pollFirst();
             System.out.println(node.val);
             if (node.left != null) {
-                level.add(node.left.val);
+                queue.add(node.left);
             }
             if (node.right != null) {
-                level.add(node.right.val);
+                queue.add(node.right);
             }
         }
+    }
 
+    //求深度的时候需要用的for循环
+    public List<List<Long>> levelOrder2(TreeNode root) {
+        List<List<Long>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        int depth = 1;
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Long> level = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.pollFirst();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
+            depth++;
+        }
+        return res;
     }
 }
