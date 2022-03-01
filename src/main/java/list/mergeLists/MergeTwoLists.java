e46be8662263604a444dd5fc728e48b9bd814239
7Savage <wjw1445331723@gmail.com>
2022-03-01 22:02:35 +0800

    写了一点东西

--- src/main/java/list/mergeLists/MergeTwoLists.java
+++ src/main/java/list/mergeLists/MergeTwoLists.java
@@ -29,4 +29,21 @@ public class MergeTwoLists {
         }
         return dummy.next;
     }
+
+    //递归：时间复杂度：O(n + m)  空间复杂度：O(n + m)
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+    }
+
+
 }
