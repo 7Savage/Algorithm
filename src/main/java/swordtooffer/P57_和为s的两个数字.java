1aeb6bac510e7869125dd183134f49bd1ea92bf0
7Savage <wjw1445331723@gmail.com>
2022-07-18 19:51:23 +0800

    添加跳表

--- src/main/java/swordtooffer/P57_和为s的两个数字.java
+++ src/main/java/swordtooffer/P57_和为s的两个数字.java
@@ -1,20 +1,22 @@
 package swordtooffer;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+
 //输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
 public class P57_和为s的两个数字 {
     //和两数之和类似，只不过这题是排序好的
     //时间和空间复杂度均为 O(N)
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int num = target - nums[i];
-            if (map.containsKey(num)) {
-                return new int[]{nums[i], num};
-            } else {
-                map.put(nums[i], num);
+        Set<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            int num=target-nums[i];
+            if(set.contains(num)){
+                return new int[]{num,nums[i]};
             }
+            set.add(nums[i]);
         }
         return new int[0];
     }
